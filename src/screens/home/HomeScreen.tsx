8b26dbf2e7f7c4264837c94486e3b9e5d543ef7b
Konstantin Barbin <konstantin.barbin@partners.fortum.com>
2021-08-31 17:02:39 +0300

    added flat list

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,16 +1,44 @@
 
 import React from "react";
 import { useEffect } from "react";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, FlatList, StyleSheet, SafeAreaView} from "react-native";
 import { connect } from "react-redux";
 import { getEstablishments } from "../../redux/actions/EstablishmentActions";
 
 function Home(props) {
-    useEffect(() =>{
-       props.getEstablishments(0)
+    useEffect(() => {
+
     })
-    return (<SafeAreaView><Text>HOME</Text></SafeAreaView>)
+    const Item = ({ title }) => (
+        <View style={styles.item}>
+            <Text style={styles.title}>{title}</Text>
+        </View>
+    );
+    const renderItem = ({ item }) => (
+        <Item title={item.title} />
+    );
+    return (<SafeAreaView style = {styles.container}>
+        <FlatList
+            data={props.getEstablishments(0)}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+        /></SafeAreaView>)
 }
 
-export default connect(null, {getEstablishments})(Home)
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    item: {
+      backgroundColor: '#f9c2ff',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 32,
+    },
+  });
+  
+
+export default connect(null, { getEstablishments })(Home)
